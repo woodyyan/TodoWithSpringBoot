7b112a781b1f3f413477dc30463d1e6bd02c5cc1
WoodyYan <sbyan@CNsbyan.local>
2016-08-02 16:05:34 +0800

    修改返回值为ResponseEntity。

--- src/main/java/com/github/controller/TodoController.java
+++ src/main/java/com/github/controller/TodoController.java
@@ -3,7 +3,13 @@ package com.github.controller;
 import com.github.model.TodoItem;
 import com.github.repository.TodoRepository;
 import com.github.repository.TodoRepositoryImpl;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
+
 import java.util.Collection;
 
 @RestController
@@ -16,37 +22,38 @@ public class TodoController {
     }
 
     @RequestMapping("/all")
-    public Collection<TodoItem> getAllTodos() {
-        return todoItems.getAll();
+    public ResponseEntity<Collection<TodoItem>> getAllTodos() {
+        Collection<TodoItem> items = todoItems.getAll();
+        return  new ResponseEntity<>(items, HttpStatus.OK);
     }
 
     @RequestMapping("/{key}")
-    public TodoItem getByKey(@PathVariable("key") String key) {
-
-        return todoItems.find(key);
+    public ResponseEntity<TodoItem> getByKey(@PathVariable("key") String key) {
+        TodoItem item = todoItems.find(key);
+        return  new ResponseEntity<>(item, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/create")
-    public String create(@RequestBody TodoItem item) {
+    public ResponseEntity create(@RequestBody TodoItem item) {
         todoItems.add(item);
-        return "Create successful!";
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/update/{key}")
-    public String update(@PathVariable("key") String key, @RequestBody TodoItem item) {
+    public ResponseEntity update(@PathVariable("key") String key, @RequestBody TodoItem item) {
         if (item == null || !item.getKey().equals(key))
         {
-            return "error";
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
 
         TodoItem todo = todoItems.find(key);
         todoItems.update(item);
-        return "update successful";
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{key}")
-    public String delete(@PathVariable("key") String key) {
+    public ResponseEntity delete(@PathVariable("key") String key) {
         todoItems.remove(key);
-        return "delete successful";
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
